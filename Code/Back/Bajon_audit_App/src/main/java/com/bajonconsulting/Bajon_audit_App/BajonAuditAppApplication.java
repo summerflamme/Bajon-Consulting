d0d84aa949bacf4a0ba36de62205946b1d5626f0
kwadjanib <bilon.kwadjani@etu.univ-tlse2.fr>
2025-09-19 02:05:39 +0200

    Init : Initialistion de l'espace de developpement

---
+++ Code/Back/Bajon_audit_App/src/main/java/com/bajonconsulting/Bajon_audit_App/BajonAuditAppApplication.java
@@ -0,0 +1,13 @@
+package com.bajonconsulting.Bajon_audit_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BajonAuditAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BajonAuditAppApplication.class, args);
+	}
+
+}
---
+++ Code/Back/Bajon_audit_App/src/test/java/com/bajonconsulting/Bajon_audit_App/BajonAuditAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bajonconsulting.Bajon_audit_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BajonAuditAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
