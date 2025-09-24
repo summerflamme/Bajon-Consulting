7b83481dec3e696bed5b3fb7bfbbaca4b5e5ac2e
simon <s.clavel.82@gmail.com>
2025-09-24 10:57:50 +0200

    ajout footer
    ajustement du header et ajout du footer

--- Code/Front/Bajon_Consulting_Audit_App/src/App.tsx
+++ Code/Front/Bajon_Consulting_Audit_App/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 //import MenuButton from './layouts/MenuButton'
 import Header from './layouts/header'
+import Footer from './layouts/Footer'
+
 
 function App() {
   return (
     <>
       <Header />
-
+      <Footer/>
     </>
   )
 }
