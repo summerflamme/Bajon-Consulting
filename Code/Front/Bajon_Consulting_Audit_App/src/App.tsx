c78d5ef82327aba170bcc30eb467859858c7dc5b
kwadjanib <bilon.kwadjani@etu.univ-tlse2.fr>
2025-10-03 00:35:09 +0200

    Fix : hearder responsive Issues :  Création du HEADER Fixes #28

--- Code/Front/Bajon_Consulting_Audit_App/src/App.tsx
+++ Code/Front/Bajon_Consulting_Audit_App/src/App.tsx
@@ -4,13 +4,14 @@ import Header from './layouts/header'
 import Footer from './layouts/Footer'
 //import Sidebar from './layouts/Sidebar'
 import LoginPage from './feature/auth/LoginPage'
+import AuditForm from './components/AuditForm'
 
 
 function App() {
   return (
     <>
       <Header />
-      <LoginPage />
+      <AuditForm />
       <Footer />
     </>
   )
