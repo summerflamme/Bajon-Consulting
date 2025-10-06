ad68eec48a2d39a86a849bc11edba952a1181a4d
kwadjanib <bilon.kwadjani@etu.univ-tlse2.fr>
2025-10-06 21:03:10 +0200

    Fix Commit

--- Code/Front/Bajon_Consulting_Audit_App/src/routes/AppRoutes.tsx
+++ Code/Front/Bajon_Consulting_Audit_App/src/routes/AppRoutes.tsx
@@ -1,11 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BaseLayout from "../layouts/BaseLayout";
 import LoginPage from "../feature/auth/LoginPage";
-<<<<<<< Updated upstream
-import UserListPage from "../components/UserListPage";
-=======
 import AuditEditorPage from "../components/AuditEditorPage";
->>>>>>> Stashed changes
 
 export default function AppRoutes() {
     return (
@@ -13,11 +9,7 @@ export default function AppRoutes() {
             <Routes>
                 <Route element={<BaseLayout />}>
                     <Route path="/" element={<LoginPage />} />
-<<<<<<< Updated upstream
-                    <Route path="/users" element={<UserListPage />} />
-=======
                     <Route path="/audit" element={<AuditEditorPage />} />
->>>>>>> Stashed changes
                 </Route>
             </Routes>
         </Router>
