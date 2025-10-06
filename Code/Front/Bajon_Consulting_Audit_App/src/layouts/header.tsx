a6c26d4c17214411cdeaa398301a9bb6dd5673c5
[titouan] <[titouannicolasgauze@gmail.com]>
2025-10-06 14:39:11 +0200

    mise a jours

--- Code/Front/Bajon_Consulting_Audit_App/src/layouts/header.tsx
+++ Code/Front/Bajon_Consulting_Audit_App/src/layouts/header.tsx
@@ -4,7 +4,9 @@ import './layouts.css';
 function Header() {
   return (
     <header className="header">
-      <img src="../../public/logo-bajon-consulting.png" alt="Logo" className="header-logo" />
+      <a href="index.tsx">
+        <img src="../../public/logo-bajon-consulting.png" alt="Logo" className="header-logo" />
+      </a>
       <div className="header-spacer" />
       <ul className='navigation'>
         <li>Audits</li>
