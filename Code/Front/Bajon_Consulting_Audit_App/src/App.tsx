ca95ccc7229cab9cf5a1d26a6b9fa11720e67631
kwadjanib <bilon.kwadjani@etu.univ-tlse2.fr>
2025-10-06 20:16:11 +0200

    fix : retour en arriere sur le merge de simon

--- Code/Front/Bajon_Consulting_Audit_App/src/App.tsx
+++ Code/Front/Bajon_Consulting_Audit_App/src/App.tsx
@@ -1,28 +1,15 @@
-import './App.css'
-//import MenuButton from './layouts/MenuButton'
-import Header from './layouts/header'
-import Footer from './layouts/Footer'
-//import Sidebar from './layouts/Sidebar'
-//import LoginPage from './feature/auth/LoginPage'
-// import SearchBar from './components/SearchBar'
-// import LoginPage from './feature/auth/LoginPage'
-import UserForm from './components/UserForm'
+import './App.css';
 import { supabase } from './supabaseClient';
-//import AuditList from './components/AuditList';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
+  // Écoute des événements d'authentification Supabase
+  supabase.auth.onAuthStateChange((event, session) => {
+    console.log("Auth event:", event);
+    console.log("Nouvelle session:", session);
+  });
 
-supabase.auth.onAuthStateChange((event, session) => {
-  console.log("Auth event:", event);
-  console.log("Nouvelle session:", session);
-});
-
-  return (
-    <>
-      <Header />
-      <UserForm/>
-      <Footer />
-    </>
-  )
+  return <AppRoutes />; // On ne met plus Header/Footer ici
 }
-export default App
+
+export default App;
