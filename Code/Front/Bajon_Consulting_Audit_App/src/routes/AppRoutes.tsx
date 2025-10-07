1a279c0a365bd581540dd233417f471e60f071ce
kwadjanib <bilon.kwadjani@etu.univ-tlse2.fr>
2025-10-07 09:08:48 +0200

    Fix typo in import statement and add new route for UserList component

--- Code/Front/Bajon_Consulting_Audit_App/src/routes/AppRoutes.tsx
+++ Code/Front/Bajon_Consulting_Audit_App/src/routes/AppRoutes.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BaseLayout from "../layouts/BaseLayout";
 import LoginPage from "../feature/auth/LoginPage";
 import AuditEditorPage from "../components/AuditEditorPage";
+import UserList from "../components/UserListPage";
+
 
 export default function AppRoutes() {
     return (
@@ -9,6 +11,8 @@ export default function AppRoutes() {
             <Routes>
                 <Route element={<BaseLayout />}>
                     <Route path="/" element={<LoginPage />} />
+                    <Route path="/users" element={<UserList />} />
+                    
                     <Route path="/audit" element={<AuditEditorPage />} />
                 </Route>
             </Routes>
