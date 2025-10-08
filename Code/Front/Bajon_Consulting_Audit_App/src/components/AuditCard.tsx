979c11afc0bd27565394c4123646428c26b7d108
Pierre Cornu <nsipierrecornu@gmail.com>
2025-10-08 15:18:47 +0200

    Update AuditList & AuditCard (getting data from Database)

--- Code/Front/Bajon_Consulting_Audit_App/src/components/AuditCard.tsx
+++ Code/Front/Bajon_Consulting_Audit_App/src/components/AuditCard.tsx
@@ -1,27 +1,77 @@
+import { useEffect, useState } from 'react';
+import { supabase } from '../supabaseClient';
 import './components.css';
 
 function AuditCard({ audit }) {
+  const [typeoffer, setTypeOffer] = useState<any[]>([]);
+  const [typeaudit, setTypeAudit] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  // Récupération des données dans Supabase
+  useEffect(() => {
+  const fetchTypeOffer = async () => {
+    const { data, error } = await supabase
+      .from("auditoffer")
+      .select("*")
+      .eq('idauditoffer', audit.idauditoffer)
+
+    if (error) {
+      console.error("Erreur de récupération :", error);
+    } else {
+      setTypeOffer(data || []);
+    }
+    setLoading(false);
+  };
+
+  fetchTypeOffer();
+  }, []);
+
+  useEffect(() => {
+  const fetchTypeAudit = async () => {
+    const { data, error } = await supabase
+      .from("audittype")
+      .select("*")
+      .eq('idaudittype', audit.idaudittype)
+
+    if (error) {
+      console.error("Erreur de récupération :", error);
+    } else {
+      setTypeAudit(data || []);
+    }
+    setLoading(false);
+  };
+
+  fetchTypeAudit();
+  }, []);
+
+  if (loading) return <p>Chargement ...</p>;
+
+  // Affichage
   return (
-    <><div>
-      <div className="card-header">
-        <h5 className="card-title"> <strong> {audit.auditname} </strong> </h5>
-      </div>
-      <div className="card-body">
-        <p className='card-text'> <strong> Type d'Offre : </strong> TYPE_OFFRE </p>
-        <p className='card-text'> <strong> Type d'Audit : </strong> TYPE_AUDIT </p>
-        <p className='card-text'> <strong> Statut : </strong> {audit.status} </p>
-      </div>
-      <div className="card-body">
-        <a href="#" className="btn btn-primary">Consulter</a>
-        <span className='card-text'> </span>
-        <a href="#" className="btn btn-primary">Modifier</a>
-        <span className='card-text'> </span>
-        <a href="#" className="btn btn-primary">Supprimer</a>
-      </div>
-      <div className="card-footer">
-        <p className='card-text'> Créé le {audit.creationdate} par Titouan </p>
-          <p className='card-text'> Modifié le DATE_MODIF par NOM_MODIF </p>
-      </div>
+    <>
+      <div>
+        <div className="card-header">
+          <h5 className="card-title"> <strong> {audit.auditname} </strong> </h5>
+        </div>
+        <div className="card-body">
+          <p className='card-text'> Création le {audit.creationdate} par Titouan </p>
+        </div>
+        <div className="card-body">
+          <p className='card-text'> <strong> Type d'Offre : </strong> {typeoffer.map(auditoffer => (auditoffer.nameauditoffer))} </p>
+          <p className='card-text'> <strong> Type d'Audit : </strong> {typeaudit.map(audittype => (audittype.nameaudittype))} </p>
+          <p className='card-text'> <strong> Statut : </strong> {audit.status} </p>
+        </div>
+        <div className="card-body">
+          <a href="#" className="btn btn-primary">Consulter</a>
+          <span className='card-text'> </span>
+          <a href="#" className="btn btn-primary">Modifier</a>
+          <span className='card-text'> </span>
+          <a href="#" className="btn btn-primary">Supprimer</a>
+        </div>
+        <div className="card-footer">
+          <p className='card-text'> Dernière modification le : </p>
+          <p className='card-text'> DATE_MODIF à HEURE_MODIF par NOM_MODIF </p>
+        </div>
     </div>
     </>
   );
--- Code/Front/Bajon_Consulting_Audit_App/src/feature/audits/AuditListPage.tsx
+++ Code/Front/Bajon_Consulting_Audit_App/src/feature/audits/AuditListPage.tsx
@@ -1,41 +1,42 @@
 import { useEffect, useState } from 'react';
+import { supabase } from '../../supabaseClient';
 import AuditCard from '../../components/AuditCard';
 import SearchBar from '../../components/SearchBar';
-import type { Audit } from '../../types/audit';
 import './AuditListPage.css';
-import { supabase } from '../../supabaseClient';
 
 function AuditList() {
-    const [audits, setAudits] = useState<Audit[]>([]);
-    const [loading, setLoading] = useState(true);
+  const [audits, setAudits] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-    const fetchAudits = async () => {
-      const { data, error } = await supabase
-        .from("audit")
-        .select("*")
+  useEffect(() => {
+  const fetchAudits = async () => {
+    const { data, error } = await supabase
+      .from("audit")
+      .select("*")
 
-      if (error) {
-        console.error("Erreur de récupération :", error);
-      } else {
-        setAudits(data || []);
-      }
-      setLoading(false);
-    };
+    if (error) {
+      console.error("Erreur de récupération :", error);
+    } else {
+      setAudits(data || []);
+    }
+    setLoading(false);
+  };
 
-    fetchAudits();
+  fetchAudits();
   }, []);
 
-  if (loading) return <p>Chargement...</p>;
+  if (loading) return <p>Chargement ...</p>;
 
   return (
-      <><SearchBar /><div className="audit-list">
+    <>
+      <SearchBar /><div className="audit-list">
           <div className="audit-grid">
               {audits.map(audit => (
                   <AuditCard key={audit.idaudit} audit={audit} />
               ))}
           </div>
-      </div></>
+      </div>
+    </>
   );
 }
 
