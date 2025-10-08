6439a2f4945eb4861af07b790f90c3ebd448f49d
ClvSimon <s.clavel.82@gmail.com>
2025-10-08 08:53:49 +0200

    ajout de user info
    ajout de la page des informations utilisateurs

---
+++ Code/Front/Bajon_Consulting_Audit_App/src/feature/users/UserListPage.tsx
@@ -0,0 +1,30 @@
+import SearchBar from "../../components/SearchBar";
+import UserCard from "./UserCard";
+import './users.css';
+
+const users = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]; // Remplacer par de vraies données
+
+function UserList() {
+    return (
+        <>
+            <SearchBar/>
+            <div className="user-list-table">
+                <div className="user-header-row">
+                    <div className="user-cell">Nom</div>
+                    <div className="user-cell">Email</div>
+                    <div className="user-cell">UID</div>
+                    <div className="user-cell">Téléphone</div>
+                    <div className="user-cell">Créé le</div>
+                    <div className="user-cell">Dernière connexion</div>
+                    <div className="user-cell actions">Actions</div>
+                </div>
+
+                {users.map((_user, idx) => (
+                    <UserCard key={idx} />
+                ))}
+            </div>
+        </>
+    );
+}
+
+export default UserList;
