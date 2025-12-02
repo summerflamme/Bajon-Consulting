5e38aa50be4204c318da31332f47c6f9ce142823
ClvSimon <s.clavel.82@gmail.com>
2025-12-02 19:47:49 +0100

    ajout co BD .env | css de auditform

--- Code/Front/Bajon_Consulting_Audit_App/src/supabaseClient.ts
+++ Code/Front/Bajon_Consulting_Audit_App/src/supabaseClient.ts
@@ -1,7 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Récupère ces infos dans le tableau de bord Supabase : Project Settings -> API
-const SUPABASE_URL = "https://bajonconsulting.endide.com/";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+console.log("URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
+
